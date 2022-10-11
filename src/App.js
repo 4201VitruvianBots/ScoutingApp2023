fc468f0537c9faa0d93190505996fc75bf6d46f7
JosiahFu <josiahfu@gmail.com>
2022-10-11 10:11:38 -0700

    Nice layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 
 function App() {
-  return (
-      <div>
-          <h1>Welcome to my App</h1>
-          <p>It smells in here</p>
-      </div>
-  );
+    return (
+        <main>
+            <h1>Scouting PASS</h1>
+            <SignIn />
+        </main>
+    );
+}
+
+function SignIn() {
+    return (
+        <div>
+            <div className="overlay"></div>
+            <div className="popup">
+                <form>
+                    <h2>Sign In</h2>
+                    <p><label htmlFor="name">Name</label><input type="text" name="name" /></p>
+                    <p><label htmlFor="event">Event</label><input type="text" name="event" /></p>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default App;
