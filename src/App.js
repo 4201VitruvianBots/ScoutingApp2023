c0909081acf726722b1b0c852f7ff6571003ec7a
natalie w <nmwpenguin@gmail.com>
2022-10-23 15:46:29 -0700

    section headers

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ function App() {
       <header className="App-header">
         <p>Scouting App</p>
       </header>
+      <div className="section-header">
+        <p>Scouter's Info</p>
+      </div>
+      <div className="section-header">
+      <p>Autonomous</p>
+      </div>
+      <div className="section-header">
+      <p>Teleop</p>
+      </div>
+      <div className="section-header">
+      <p>Endgame</p>
+      </div>
     </div>
   );
 }
