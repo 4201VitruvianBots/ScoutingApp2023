eb2e87c0586ea10907f6d6aa72bb2e7f6651c81f
Aiden Corral <aidencorral316@gmail.com>
2022-10-25 19:52:26 -0700

    wip tab system

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import './App.css';
 import { SignIn, PreGame, Auto, TeleOp, Endgame, SavePage } from "./Pages";
-function App() {
-  return (
-    <main>
-      <SignIn  />
-      <PreGame />
-      <Auto />
-      <TeleOp />
-      <Endgame />
-      <SavePage />
-    </main>
-  );
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    this.State = {id:"Pre-Game"}
+  }
+    render() {
+      return (
+        <main>
+            <SignIn />
+            <div>
+                <TabButton tabId="pre-game">Pre-Game</TabButton>
+            </div>
+            <PreGame selected={false}/>
+            <Auto selected={false} />
+            <TeleOp selected={false} />
+            <Endgame selected={false} />
+            <SavePage selected={false} />
+        </main>
+        );
+        this.setState = this.setState.bind(this)
+
+    }   
+        
+    setState() {
+            this.setState
+           
+        }
+}
+
+function TabButton(props) {
+    <button>{props.children}</button>  
 }
 
 export default App;
