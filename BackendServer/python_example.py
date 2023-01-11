6cf922a8291ba2cd08697787b71595c68ee11c47
JosiahFu <josiahfu@gmail.com>
2023-01-10 22:48:44 -0800

    Fixed actions & more example

--- BackendServer/python_example.py
+++ BackendServer/python_example.py
@@ -14,3 +14,23 @@ funcName()
 
 for i in range(10):
     print(i)
+
+
+y = {
+    'name': 'Bob',
+    'age': 10,
+    'color': 'purple',
+    'sad': False
+}
+
+y['name']
+y['age']
+
+
+my_list = [
+    1,2,3,4,0,13,6,1,6723
+]
+
+my_list[0]
+
+my_list.append(12)
