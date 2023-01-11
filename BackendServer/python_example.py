1cda4473e994b95bd3d58a455611aea5e7169a52
JosiahFu <josiahfu@gmail.com>
2023-01-10 16:57:07 -0800

    Example python

---
+++ BackendServer/python_example.py
@@ -0,0 +1,16 @@
+print('text output')
+
+# Variable
+x = 12
+
+# Instead of curly braces use colon and indent lines
+if x == 12:
+    print('x is 12')
+
+def funcName():
+    print('Hi')
+
+funcName()
+
+for i in range(10):
+    print(i)
