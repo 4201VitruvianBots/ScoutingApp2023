8891416d58f68f9296f1f911a5316caa1ed651c3
Nathan <Nathan.DuPont01@gmail.com>
2023-01-16 17:39:26 -0800

    Revert "Revert "Merge pull request #41 from 4201VitruvianBots/37-make-button-…"

--- MatchScouting/src/Form.js
+++ MatchScouting/src/Form.js
@@ -50,6 +50,7 @@ class NumberInput extends React.Component {
         // props = {id: "teleopUp", label: "Upper Cargo"};
         return (
             <div>
+
                 <div className="labelleft">
                     <label htmlFor={this.state.id}>{this.state.label}</label>
                 </div>
@@ -65,9 +66,84 @@ class NumberInput extends React.Component {
     }
 }
 
-export { RadioButtons, NumberInput };
 
 
 
 
+class ButtonInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { id: props.id, off_label: props.off_label, value: 0,  on_label: props.on_label };
+        this.setValue = this.setValue.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.increaseValue = this.increaseValue.bind(this);
+        this.decreaseValue = this.decreaseValue.bind(this);
+        this.setValueFinal = this.setValueFinal.bind(this);
+
+    }
+
+    setValue(value) {
+        this.setState({ value: Math.abs(parseInt(value)) });
+    }
+
+    handleChange(event) {
+        this.setValue(event.target.value);
+    }
+
+    increaseValue() {
+        this.setState({ value: this.state.value + 1 });
+    }
+
+    decreaseValue() {
+        this.setState({ value: this.state.value - 1 });
+    }
+    setValueFinal() {
+        if (this.state.value === 0) {
+            this.increaseValue();
+        } else if (this.state.value === 1) {
+            this.decreaseValue();
+        }
+    }
+
+
+
+    render() {
+        if (this.state.value === 0) {
+            return (
+                // <div>
+
+                    <div className="ToggleButton">
+                        <input type="button" className="number-off" value={this.state.off_label} onClick={this.setValueFinal} />
+
+                    {/* </div> */}
+                </div>
+
+            );
+        } else if (this.state.value === 1) {
+            return (
+                // <div>
+
+
+                    <div className="ToggleButton">
+                        <input type="button" className="number-on" value={this.state.on_label} onClick={this.setValueFinal} />
+                    </div>
+                // </div>
+            );
+        }
+        // return (
+        //     <div>
+        //         <div className="labelleft">
+        //             <label htmlFor={this.state.id}>{this.state.label}</label>
+        //             </div>
+
+        //         <div className = "buttonright">
+        //             <input type="button" className="number-input" value={this.state.value} onClick={this.setValueFinal}/>     
+        //     </div>
+        //     </div>
+
+        // );
+    }
+}
+
+export { RadioButtons, NumberInput, ButtonInput };
 
