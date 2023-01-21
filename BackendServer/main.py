6bc57f51b99f35732efe68b1606742c420c2bbe3
vanessa <dragneelmegumin23@gmail.com>
2023-01-21 14:22:53 -0800

    Store and retrieve data in database

--- BackendServer/main.py
+++ BackendServer/main.py
@@ -1,26 +1,59 @@
 from flask import Flask, request
+import mysql.connector
+import atexit
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="my-secret-pw",
+  database="rawData"
+)
+
+columns = ('Match_Number', 'Team_Number', 'Scouter_Name', 'Team_Alliance', 'Competition', 'Mobility', 'Show_Time', 'Auto_Cube_Low', 'Auto_Cube_Mid', 'Auto_Cube_High', 'Auto_Cone_Low', 'Auto_Cone_Mid', 'Auto_Cone_High', 'Auto_Station', 'Tele_Cube_Low', 'Tele_Cube_Mid', 'Tele_Cube_High', 'Tele_Cone_Low', 'Tele_Cone_Mid', 'Tele_Cone_High', 'Tele_Station', 'Comments')
+
+mycursor = mydb.cursor()
 
 app = Flask(__name__)
 match_list=[]
 @app.route('/data', methods=['GET'])
 def handle_get():
     # Handle GET request
-    data = 'This is a GET request'
-    print(request)
-    return match_list
+    mycursor.execute("SELECT * FROM matchData")
+    rows = mycursor.fetchall()
+    return [dict(zip(columns, row)) for row in rows]
+
 
 @app.route('/data', methods=['POST'])
 def handle_post():
     # Handle POST request\
     formData = request.form
     # data = request.get_json()
-    print(request)
-    print(formData)
-    match_list.append(formData)
+    # match_list.append(formData)
+
+    mycursor.execute('INSERT INTO matchData({}) VALUES ({})'.format(
+        ', '.join(formData.keys()),
+        ', '.join(['%s'] * len(formData))
+    ), tuple([format_data(formData[key], key) for key in formData]))
+
+    mydb.commit()
     #for i in variable:
        # print(i)
     # Do something with the data
     return 'Data received'
 
+
+def format_data(string, name):
+    if name in ('Scouter_Name', 'Competition', 'Comments'):
+        return string
+    if name in ('Mobility', 'Show_Time'):
+        return bool(string)
+    return int(string)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+def exit_handler():
+    mycursor.close()
+    mydb.close()
+
+atexit.register(exit_handler)
--- BackendServer/rawDataTable.sql
+++ BackendServer/rawDataTable.sql
@@ -1,25 +1,26 @@
 USE rawData;
 
-CREATE TABLE rawData(
-	Match_Number BIGINT NULL,
-	Team_number BIGINT NULL,
+CREATE TABLE matchData(
+	Match_Number INT NULL,
+	Team_Number INT NULL,
 	Scouter_Name TEXT NULL,
 	Team_Alliance INT NULL,
 	Competition TEXT NULL,
-	Mobility INT NULL,
+	Mobility BOOLEAN NULL,
+	Show_Time BOOLEAN NULL,
 	Auto_Cube_low INT NULL,
 	Auto_Cube_Mid INT NULL,
 	Auto_Cube_High INT NULL,
 	Auto_Cone_low INT NULL,
 	Auto_Cone_Mid INT NULL,
 	Auto_Cone_High INT NULL,
+	Auto_Station INT NULL,
 	Tele_Cube_low INT NULL,
 	Tele_Cube_Mid INT NULL,
 	Tele_Cube_High INT NULL,
 	Tele_Cone_low INT NULL,
 	Tele_Cone_Mid INT NULL,
 	Tele_Cone_High INT NULL,
-	Auto_Station INT NULL,
 	Tele_Station INT NULL,
-	Comments LONGTEXT NULL
+	Comments TEXT NULL
 );
