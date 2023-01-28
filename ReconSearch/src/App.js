759396bb9ded645d27519f8bb28fd43b751c1c75
CrisantoCarillo <crisanto.e.carrillo@gmail.com>
2023-01-28 12:43:22 -0800

    Fixed buttons, added event listeners, formatting

--- ReconSearch/src/App.js
+++ ReconSearch/src/App.js
@@ -6,51 +6,52 @@ import QRCode from 'react-qr-code';
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {signedIn: false, ScouterName:"", EventName:""};
+        this.state = { signedIn: false, ScouterName: "", EventName: "" };
         this.setSelected = this.setSelected.bind(this);
         this.SignInHandler = this.SignInHandler.bind(this)
         this.SubmitHandler = this.SubmitHandler.bind(this)
-
     }
 
     SignInHandler(e) {
         e.preventDefault();
         const answers = e.target.elements;
-        this.setState({signedIn: true, ScouterName: answers.Sname.value, EventName: answers.Ename.value, QRCode: null});
+        this.setState({ signedIn: true, ScouterName: answers.Sname.value, EventName: answers.Ename.value, QRCode: null });
         return false;
     }
 
     SubmitHandler(e) {
         e.preventDefault();
         const answers = e.target.elements;
-        this.setState({QRCode: <QRCode value={
-            `${this.state.ScouterName}\t` +
-            `${this.state.EventName}\t` +
-            `${answers.match.value}\t` +
-            `${answers.Num.value}\t` +
-            `${answers.Alliance.value}\t` +
-            `${answers.GeneralUp.value}\t` +
-            `${answers.GeneralLow.value}\t` +
-            `${answers.PhotosUp.value}\t` +
-            `${answers.PhotosLow.value}\t` +
-            `${answers.foul.value}\t` +
-            `${answers.tfoul.value}\t` +
-            `${answers.climbType.value}\t` +
-            `${answers.notes.value}`
-        } size={512} />});
+        this.setState({
+            QRCode: <QRCode value={
+                `${this.state.ScouterName}\t` +
+                `${this.state.EventName}\t` +
+                `${answers.match.value}\t` +
+                `${answers.Num.value}\t` +
+                `${answers.Alliance.value}\t` +
+                `${answers.GeneralUp.value}\t` +
+                `${answers.GeneralLow.value}\t` +
+                `${answers.PhotosUp.value}\t` +
+                `${answers.PhotosLow.value}\t` +
+                `${answers.foul.value}\t` +
+                `${answers.tfoul.value}\t` +
+                `${answers.climbType.value}\t` +
+                `${answers.notes.value}`
+            } size={512} />
+        });
         return false;
     }
 
     setSelected(id) {
-        this.setState({selected: id});
+        this.setState({ selected: id });
     }
 
     render() {
-      return (
-        <main>
-            {/* <p className="page-title">Is that the roof?!</p> */}
-            {this.state.signedIn || <SignIn onSubmit={this.SignInHandler}/>}
-            {/*
+        return (
+            <main>
+                {/* <p className="page-title">Is that the roof?!</p> */}
+                {this.state.signedIn || <SignIn onSubmit={this.SignInHandler} />}
+                {/*
             <div >
                 <TabButton headerButtonsonClick={this.setSelected} tabId="pre-game">Pre-Game</TabButton>
                 <TabButton onClick={this.setSelected} tabId="General">General</TabButton>
@@ -59,20 +60,20 @@ class App extends React.Component {
                 <TabButton onClick={this.setSelected} tabId="save-page">Save</TabButton>
             </div>
       */}
-            <form onSubmit={this.SubmitHandler} action="#"> 
-                <TeamInfo  selected={this.state.selected === 'teaminfo'}/>
-                <General selected={this.state.selected === 'general'} />
-                <Photos selected={this.state.selected === 'photos'} />
-                
-                <SavePage selected={this.state.selected === 'save-page'} QRCode={this.state.QRCode} />
-            </form>
-           
-        </main>
+                <form onSubmit={this.SubmitHandler} action="#">
+                    <TeamInfo selected={this.state.selected === 'teaminfo'} />
+                    <General selected={this.state.selected === 'general'} />
+                    <Photos selected={this.state.selected === 'photos'} />
+
+                    <SavePage selected={this.state.selected === 'save-page'} QRCode={this.state.QRCode} />
+                </form>
+
+            </main>
         );
-    }  
-    
+    }
+
     QRcodeGenerator() {
-        
+
     }
 }
 
