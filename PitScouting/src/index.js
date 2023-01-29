3d5baaa764f58fd3e167a336398b524f9758ec52
natalie w <nmwpenguin@gmail.com>
2023-01-28 16:05:32 -0800

    it's almost working
    :D

--- PitScouting/src/index.js
+++ PitScouting/src/index.js
@@ -4,6 +4,9 @@ import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
