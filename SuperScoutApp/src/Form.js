cbf14de0018f887aec4f6b7bc57dfbcd747f2d13
Crisanto <crisanto.e.carrillo@gmail.com>
2023-02-05 16:53:49 -0800

    Made a new SuperScouting App folder

---
+++ SuperScoutApp/src/Form.js
@@ -0,0 +1,192 @@
+import React from "react";
+import './App.css';
+
+// Radio Buttons
+function RadioButtons(props) {
+    let output = [];
+    for (let item in props.items) {
+        output.push(
+            <p key={item}>
+                <label className="label-size">
+                    <input className="align-radio" type="radio" name={props.name} value={item} />
+                    {props.items[item]}
+                </label>
+            </p>
+        )
+    }
+    return output;
+
+}
+
+// Number input
+class NumberInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { id: props.id, label: props.label, value: 0 };
+        this.setValue = this.setValue.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.increaseValue = this.increaseValue.bind(this);
+        this.decreaseValue = this.decreaseValue.bind(this);
+        // eslint-disable-next-line no-restricted-globals
+        addEventListener('reset', () => { this.setState({ value: 0 }) })
+    }
+
+
+    setValue(value) {
+        this.setState({ value: Math.abs(parseInt(value)) });
+    }
+
+    handleChange(event) {
+        this.setValue(event.target.value);
+    }
+
+    increaseValue() {
+        this.setState({ value: this.state.value + 1 });
+    }
+
+    decreaseValue() {
+        if (this.state.value > 0)
+            this.setState({ value: this.state.value - 1 });
+    }
+
+    render() {
+        // props = {id: "teleopUp", label: "Upper Cargo"};
+        return (
+            <div>
+
+                <div className="labelleft">
+                    <label htmlFor={this.state.id}>{this.state.label}</label>
+                </div>
+                <div className="buttonright">
+                    <input type="button" className="chonk" value="-" onClick={this.decreaseValue} />
+                    <input type="number" value={this.state.value} onChange={this.handleChange} name={this.state.id} min="0" />
+                    <input type="button" className="chonk" value="+" onClick={this.increaseValue} />
+                </div>
+            </div>
+
+
+        );
+    }
+}
+
+class ButtonInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { id: props.id, off_label: props.off_label, value: 0, on_label: props.on_label };
+        this.setValue = this.setValue.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.increaseValue = this.increaseValue.bind(this);
+        this.decreaseValue = this.decreaseValue.bind(this);
+        this.setValueFinal = this.setValueFinal.bind(this);
+        // eslint-disable-next-line no-restricted-globals
+        addEventListener('reset', () => { this.setState({ value: 0 }) })
+    }
+
+    setValue(value) {
+        this.setState({ value: Math.abs(parseInt(value)) });
+    }
+
+    handleChange(event) {
+        this.setValue(event.target.value);
+    }
+
+    increaseValue() {
+        this.setState({ value: this.state.value + 1 });
+    }
+
+    decreaseValue() {
+        this.setState({ value: this.state.value - 1 });
+    }
+    setValueFinal() {
+        if (this.state.value === 0) {
+            this.increaseValue();
+        } else if (this.state.value === 1) {
+            this.decreaseValue();
+        }
+    }
+    render() {
+        if (this.state.value === 0) {
+            return (
+                <div className="ToggleButton">
+                    <input type="hidden" value={false} name={this.state.id} />
+                    <input type="button" className="number-off" value={this.state.off_label} onClick={this.setValueFinal} />
+                </div>
+            );
+        } else if (this.state.value === 1) {
+            return (
+                <div className="ToggleButton">
+                    <input type="hidden" value={true} name={this.state.id} />
+                    <input type="button" className="number-on" value={this.state.on_label} onClick={this.setValueFinal} />
+                </div>
+            );
+        }
+    }
+}
+
+
+class MultiButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { items: props.items, id: props.id, label: props.label, value: 0, selected: 0 };
+        this.test1 = this.test1.bind(this);
+        this.generateButtons = this.generateButtons.bind(this);
+    }
+
+    test1(id) {
+        this.setState({
+            selected: id
+        })
+        console.log('I\'ve been called ' + (id));
+    }
+
+    generateButtons() {
+        let output = [];
+
+        for (let index in this.state.items) {
+
+            let component;
+
+            if (this.state.selected === index) {
+                component = <input key={index} type="button" className="number-on" value={this.state.items[index][0]} onClick={() => this.test1(index)} />
+                // component = <input type="button" value={1} on_label={(this.state.items[index][0])} off_label={(this.state.items[index][1])} id={index} test1={this.test1} />
+                console.log('Selected button generated');
+
+            } else {
+                // component = <input type="button" value={0} on_label={(this.state.items[index][0])} off_label={(this.state.items[index][1])} id={index} test1={this.test1} />
+                component = <input key={index} type="button" className="number-off" value={this.state.items[index][1]} onClick={() => this.test1(index)} />
+                console.log('Not selected button generated');
+            }
+
+            output.push(component)
+
+
+        }
+        return output;
+    }
+
+
+    render() {
+
+        return (
+            <div>
+                <this.generateButtons />
+            </div>
+        )
+
+    }
+}
+
+class Upload extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { file: null }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(event) {
+        this.setState({
+            file: URL.createObjectURL(event.target.files[0])
+        })
+    }
+}
+
+export { RadioButtons, NumberInput, ButtonInput, MultiButton };
---
+++ SuperScoutApp/src/Pages.js
@@ -0,0 +1,203 @@
+import { RadioButtons, NumberInput, ButtonInput, MultiButton } from "./Form";
+import './App.css';
+
+function Page(props) {
+    return (
+        <div className={props.selected ? 'page selected' : 'page'} id={props.id}>
+            {props.children}
+        </div>
+    );
+}
+
+function Navigation(props) {
+    return (
+        <Page selected={props.selected} className="page" id="navigation">
+            <ul>
+                <li><a href="#SignIn">Sign-In</a></li>
+                <li><a href="#Pre">Pre-Game</a></li>
+                <li><a href="#Auto">Auto</a></li>
+                <li><a href="#Tele">Tele-Op</a></li>
+                <li><a href="#Save">Submit</a></li> {/*change the rest of the code to 'submit' as well? does it matter?*/}
+            </ul>
+        </Page>
+    );
+}
+
+function SignIn(props) {
+    return (
+        <Page selected={props.selected} className="page" id="sign-in">
+            <div>
+                <p className="section-label" id="SignIn">Energized</p>
+
+                <div className="textArea">
+                    <input type="text" id="Scouter_Name" name="Scouter_Name" placeholder="Scouter Name" className="textInput" />
+                    <select name="Competition" id="Competition" defaultValue="Choose">
+                        <option value="Choose" className="Placeholder" disabled>Choose Event</option>
+                        <option value="Hueneme">Port Hueneme</option>
+                    </select>
+                    <input type="submit" className="save-button" value="Sign In" />
+                </div>
+            </div>
+        </Page>
+    );
+}
+
+function PreGame(props) {
+    return (
+        <Page selected={props.selected} className="page" id="pre-game">
+            <p className="section-label" id="Pre">Pre-Game</p>
+
+            <div className="textArea">
+
+                <p className="itemLabel" htmlFor="Match_Number">MATCH NUMBER</p>
+                <input type="number" id="Match_Number" name="Match_Number" className="textInput" />
+
+                <p className="itemLabel" htmlFor="Match_Number">TEAM NUMBER</p>
+                <input type="number" id="Team_Number" name="Team_Number" className="textInput" />
+
+
+                <p className="itemLabel" htmlFor="Match_Number">ALLIANCE</p>
+                <div className="allianceSelect">
+                    <MultiButton items={[['Blue 1', 'BLUE 1'], ['Blue 2', 'BLUE 2'], ['Blue 3', 'BLUE 3'], ['Red 1', 'RED 1'], ['Red 2', 'RED 2'], ['Red 3', 'RED 3']]} name="autostation" />
+
+                </div>
+                {/* Off value at index 0, ON value at index 1 */}
+
+
+                {/* <h1><strong>No show robot?</strong></h1>
+                 <ButtonInput id="Show_Time" on_label="Robot did not show" off_label="Robot showed" />
+                <div className="one">
+                    <MultiButton items={[['Showed up', 'Didn\'t show up']]} name="noShow" />
+                </div> */}
+                <div></div>
+            </div>
+        </Page>
+
+    );
+}
+
+
+function Auto(props) {
+    return (
+        <Page selected={props.selected} id="auto">
+            <p className="section-label" id="Auto">Auto</p>
+            <div className="textArea">
+
+                <ButtonInput on_label='Mobility' off_label='Mobility' id="Mobility" />
+                {/* <button type="button">Mobility?</button> */}
+
+                {/* <h2>Mobility</h2> */}
+                {/* <div className="container">
+                    <div className="center">
+                        <button>Mobility</button>
+                    </div>
+                </div> 
+                <br></br>
+                <input type="checkbox" name="mobility"></input>
+                <label htmlFor="mobility" className="label-size">Mobility</label> */}
+                <br />
+                <p className="itemLabel">CHARGING STATION</p>
+                <div className="align-radio">
+                    <MultiButton items={[['Docked', 'DOCKED'], ['Engaged', 'ENGAGED'], ['No points', 'NO POINTS']]} name="Auto_Station" />
+
+                </div>
+                <div className="gallery">
+                    <div>
+                        <h1>Cones</h1>
+                        <NumberInput id="Auto_Cone_High" />
+                        <NumberInput id="Auto_Cone_Mid" />
+                        <NumberInput id="Auto_Cone_Low" />
+                    </div>
+                    <div>
+                        <h1>Cubes</h1>
+                        <NumberInput id="Auto_Cube_High" />
+                        <NumberInput id="Auto_Cube_Mid" />
+                        <NumberInput id="Auto_Cube_Low" />
+                    </div>
+                </div>
+            </div>
+        </Page>
+    );
+}
+
+function TeleOp(props) {
+    return (
+        <Page selected={props.selected} id="Tele">
+            <p className="section-label">Teleop/Endgame</p>
+
+            <div className="textArea">
+
+                <div className="gallery">
+                    <div>
+                        <h1>Cones</h1>
+                        <NumberInput id="Auto_Cone_High" />
+                        <NumberInput id="Auto_Cone_Mid" />
+                        <NumberInput id="Auto_Cone_Low" />
+                    </div>
+                    <div>
+                        <h1>Cubes</h1>
+                        <NumberInput id="Auto_Cube_High" />
+                        <NumberInput id="Auto_Cube_Mid" />
+                        <NumberInput id="Auto_Cube_Low" />
+                    </div>
+                </div>
+
+                {/* <h1>Cones</h1>
+                <h2>High</h2>
+                <NumberInput items={['1']} />
+                <h2>Mid</h2>
+                {/* <br></br> */}
+
+            </div>
+
+
+            <div className="textArea">
+                <h1>Charging Station</h1>
+                <div className="align-radio">
+                    <MultiButton items={[['Docked', 'DOCKED'], ['Engaged', 'ENGAGED'], ['No points', 'NO POINTS'], ['Parked', 'PARKED']]} name="Tele_Station" />
+                </div>
+
+            </div>
+        </Page>
+    );
+}
+// charge station
+// button for mobility
+function SavePage(props) {
+    return (
+
+        <Page selected={props.selected} id="save-page">
+
+
+            <p className="section-label" id="Save">Submit</p>
+            <div className="textArea">
+                {/* <label htmlFor="notes" className="item-label">Notes</label> */}
+                {/* <h1>Some things you could comment are:</h1>
+                <div className="bullet-points">
+
+                    <li className="questions">Placeholder Questions?</li>
+                    <li className="questions">Is it successful?</li>
+                    <li className="questions">Does it move?</li>
+                    <li className="questions">Conesistent?</li>
+                    <li className="questions">Can the robot?</li>
+                    <li className="questions">Does it?</li>
+                </div>
+                <br /> */}
+
+                <input type="text" id="Comments" name="Comments" placeholder="Comment here" className="textInput" />
+                {/* <label className="item-label" htmlFor="clear">QR code and clear</label>
+                <input type="submit" className="SAVE" value="Generate QR code"></input>
+                <br />
+                <label className="item-label" htmlFor="continue">Save and continue</label>
+                 */}
+                <input type="submit" className="submit-button"></input>
+                {/* <input type="reset" className="CLEAR" value="Clear Form" /> */}
+                {/* <div id="QRCode">{props.QRCode}</div> */}
+
+            </div>
+
+        </Page>
+    );
+}
+
+export { SignIn, PreGame, Auto, TeleOp, SavePage, Navigation };
