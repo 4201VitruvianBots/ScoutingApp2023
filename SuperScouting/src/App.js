f2ccd0855ed8e7a9d5b80e4c9f898722af5aff7c
natalie w <nmwpenguin@gmail.com>
2023-02-08 10:09:37 -0800

    set up SuperScouting folder

---
+++ SuperScouting/src/App.js
@@ -0,0 +1,93 @@
+import './App.css';
+import { SignIn, TeamInfo, General, Photos, SavePage } from "./Pages";
+import React from "react";
+import QRCode from 'react-qr-code';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {signedIn: false, ScouterName:"", EventName:""};
+        this.setSelected = this.setSelected.bind(this);
+        this.SignInHandler = this.SignInHandler.bind(this)
+        this.SubmitHandler = this.SubmitHandler.bind(this)
+
+    }
+
+    SignInHandler(e) {
+        e.preventDefault();
+        const answers = e.target.elements;
+        this.setState({signedIn: true, ScouterName: answers.Sname.value, EventName: answers.Ename.value, QRCode: null});
+        return false;
+    }
+
+    SubmitHandler(e) {
+        e.preventDefault();
+        const answers = e.target.elements;
+        this.setState({QRCode: <QRCode value={
+            `${this.state.ScouterName}\t` +
+            `${this.state.EventName}\t` +
+            `${answers.match.value}\t` +
+            `${answers.Num.value}\t` +
+            `${answers.Alliance.value}\t` +
+            `${answers.GeneralUp.value}\t` +
+            `${answers.GeneralLow.value}\t` +
+            `${answers.PhotosUp.value}\t` +
+            `${answers.PhotosLow.value}\t` +
+            `${answers.foul.value}\t` +
+            `${answers.tfoul.value}\t` +
+            `${answers.climbType.value}\t` +
+            `${answers.notes.value}`
+        } size={512} />});
+        return false;
+    }
+
+    setSelected(id) {
+        this.setState({selected: id});
+    }
+
+    render() {
+      return (
+        <main>
+            <p className="page-title">Welcome to Vitruvian Scouting</p>
+            {<SignIn onSubmit={this.SignInHandler}/>}
+
+            {/*
+            <div >
+                <TabButton headerButtonsonClick={this.setSelected} tabId="pre-game">Pre-Game</TabButton>
+                <TabButton onClick={this.setSelected} tabId="General">General</TabButton>
+                <TabButton onClick={this.setSelected} tabId="tele-op">Photos</TabButton>
+                <TabButton onClick={this.setSelected} tabId="endgame">Endgame</TabButton>
+                <TabButton onClick={this.setSelected} tabId="save-page">Save</TabButton>
+            </div>
+      */}
+              <form action="http://127.0.0.1:5000/data/pits" method="POST" target="frame" id="myForm" onSubmit={clearForm}>
+                <input type='hidden' value={this.state.EventName} name='Competition' />
+                <input type='hidden' value={this.state.ScouterName} name='Scouter_Name' />
+                <TeamInfo  selected={this.state.selected === 'teaminfo'}/>
+                <General selected={this.state.selected === 'general'} />
+                <Photos selected={this.state.selected === 'photos'} />
+                <SavePage selected={this.state.selected === 'save-page'} QRCode={this.state.QRCode} />
+
+            </form>
+            <iframe name="frame" title="frame"></iframe>
+           
+        </main>
+        );
+    }  
+}
+
+function clearForm() { 
+    document.getElementById("myForm").submit();
+    setTimeout(function() {
+        document.getElementById("myForm").reset();
+        window.location.href="#SignIn"
+    }, 0)
+} 
+
+// function TabButton(props) {
+//     return <button onClick={() => props.onClick(props.tabId)}>{props.children}</button>;
+// }
+
+
+
+export default App;
---
+++ SuperScouting/src/Pages.js
@@ -0,0 +1,165 @@
+import { RadioButtons, NumberInput, ButtonInput, MultiButton, Upload } from "./Form";
+import './App.css';
+
+
+function Page(props) {
+    return (
+        <div className={props.selected ? 'page selected' : 'page'} id={props.id}>
+            {props.children}
+        </div>
+    );
+}
+
+
+function SignIn(props) {
+    return (
+        <div>
+            <p className="section-label">Pit Scouting</p>
+            <form onSubmit={props.onSubmit} action="#">
+                <div className="textArea">
+                    <input type="text" id="Sname" name="Scouter_Name" placeholder="Scouter Name" />
+                    <label className="item-label" htmlFor="Ename"><strong>Event Name</strong> </label>
+                    <select name="Competition" id="Ename" defaultValue="Choose">
+                        <option value="Choose" className="Placeholder" disabled>Choose Event</option>
+                        <option value="Port Hueneme">Port Hueneme</option>
+                    </select>
+                    <input type="submit" className="SAVE" value="Sign In" />
+                </div>
+            </form>
+        </div>
+    );
+}
+
+function TeamInfo(props) {
+    return (
+        <Page selected={props.selected} className="page" id="teaminfo">
+            <p className="section-label">Team Info</p>
+            <div className="textArea">
+                <input className="text-input" type="text" id="Num" name="Team_Number" placeholder="Team NUMBER" />
+                <input className="text-input" type="text" id="Num" name="Team_Name" placeholder="Team NAME" />
+                <RadioButtons />
+            </div>
+        </Page>
+    );
+}
+
+function General(props) {
+    return (
+        <Page selected={props.selected} id="general">
+            <p className="section-label">General</p>
+            <div className="textArea">
+                <div className="gallery">
+
+                    <div className="drivetrain">
+                        <p className="generalLabel">Drivetrain Type</p>
+                        {/* some kind of input where you can declare the number and titles of all your options in one input*/}
+                        {/* <MultiButton />
+                        <MultiButton />
+                        <MultiButton />
+                        <MultiButton /> */}
+                        <MultiButton items={[['Tank', 't'], ['Swerve', 's'], ['Mecanum', 'm'], ['Other', 'o']]} id="DriveTrain" />
+                    </div>
+
+                    
+                    {/* <div className="test2" role="group" aria-label="Basic radio toggle button group">
+                        <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked />
+                        <label class="btn btn-outline-primary" for="btnradio1">Radio 1</label>
+
+                        <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off" />
+                        <label class="btn btn-outline-primary" for="btnradio2">Radio 2</label>
+
+                        <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off" />
+                        <label class="btn btn-outline-primary" for="btnradio3">Radio 3</label>
+                    </div> */}
+
+                    <div className="gamepieces">
+                        <p className="generalLabel">Game Piece Capability</p>
+                        <ButtonInput on_label='CONES' off_label='Cones' id='Can_Hold_Cone' />
+                        <ButtonInput on_label='CUBES' off_label='Cubes' id='Can_Hold_Cube' />
+                    </div>
+
+                    <div className="scoringLocation">
+                        <p className="generalLabel">Scoring Location Capability</p>
+                        <ButtonInput on_label='LOW' off_label='Low' id='1' value="1" />
+                        <ButtonInput on_label='MID' off_label='Mid' id='2' value="1" />
+                        <ButtonInput on_label='HIGH' off_label='High' id='3' value="1" />
+
+                    </div>
+
+                    <div className="motors">
+                        <p className="generalLabel"># of Motors (Tank- on each side)</p>
+                        <NumberInput id="Number_Of_Motors" />
+                    </div>
+
+                    <div className="batteries">
+                        <p className="generalLabel"># of Batteries (total)</p>
+                        <NumberInput id="Number_Of_Batteries" />
+                    </div>
+
+                    <div className="motorType">
+                        <input type="text" placeholder="Drivetrain Motor Type" name="DriveTrain_Motor_Type" ></input>
+                    </div>
+
+                    <div className="autos">
+                        <input type="text" placeholder="Autos (# and type)" name="Autos"></input>
+                    </div>
+
+                    <div className="workingOn">
+                        <input type="text" placeholder="They're working on..." name="Working_On" ></input>
+                    </div>
+
+                </div>
+            </div>
+        </Page>
+    );
+}
+
+
+
+function Photos(props) {
+    return (
+        <Page selected={props.selected} id="photos">
+            <p className="section-label">Photos</p>
+            <div className="textArea">
+                <p>DRIVETRAIN</p>
+                <Upload size="100px"></Upload>
+                <p>INTAKE</p>
+                <Upload></Upload>
+                <p>UPTAKE</p>
+                <Upload></Upload>
+                <p>OUTTAKE</p>
+                <Upload></Upload>
+                <p>EXTRAS</p>
+                <Upload></Upload>
+            {/* <input type="file" multiple accept="image/*" /> */}
+            </div>
+        </Page>
+    );
+}
+
+function SavePage(props) {
+    return (
+        <Page selected={props.selected} id="save-page">
+            
+            <p className="section-label">Save Page</p>
+            <div className="textArea">
+            <label htmlFor="Comments" className="item-label">Notes</label>
+                <br/>
+                <br/>
+                <input type="text" id="notes" name="Comments" />
+                
+             {/* <label className="item-label" htmlFor="continue">Save and continue</label>  */}
+            
+            <input type="submit" className="submit-button"/>Save and continue
+
+            <input type="reset" className="CLEAR" value="Clear Form"/>
+            {/* </div> */}
+
+            {/* <div id="QRCode">{props.QRCode}</div>                
+            </div> */}
+            </div>
+        </Page>
+    );
+}
+
+export { SignIn, TeamInfo, General, Photos, SavePage };
