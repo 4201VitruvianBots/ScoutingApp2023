1f6d7367dc1a8fb81fb875838d9336ed0d2510ad
JosiahFu <josiahfu@gmail.com>
2023-02-26 09:31:22 -0800

    CSV download

--- PitScouting/src/App.js
+++ PitScouting/src/App.js
@@ -1,50 +1,74 @@
 import './App.css';
 import { SignIn, General, Photos, SavePage } from "./Pages";
 import React from "react";
-import QRCode from 'react-qr-code';
+
+const fields = [
+    'Scouter_Name',
+    'Competition',
+    'Team_Number',
+    'DriveTrain',
+    'Can_Hold_Cone',
+    'Can_Hold_Cube',
+    'Low',
+    'Mid',
+    'High',
+    'Number_Of_Motors',
+    'Number_Of_Batteries',
+    'DriveTrain_Motor_Type',
+    'Autos',
+    'Working_On',
+    // 'Drivetrain_Photo',
+    // 'Intake_Photo',
+    // 'Uptake_Photo',
+    // 'Outtake_Photo',
+    // 'Extras_Photo',
+    'Comments'
+];
+
+function download(data, title) {
+    const blob = new Blob([data], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.download = title;
+    link.href = url;
+    link.click();
+}
+
+function csvStringify(data) {
+    console.log(data);
+    return data.map(e => (
+        e.map(e2 => {
+            if (e2.includes('"') || e2.includes('\n') || e2.includes('\r') || e2.includes(',')) {
+                return '"' + e2.replaceAll('"', '""') + '"';
+            }
+            return e2;
+        }).join(',') + '\r\n'
+    )).join('');
+}
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = { signedIn: false, ScouterName: "", EventName: "" };
-        this.setSelected = this.setSelected.bind(this);
-        this.SignInHandler = this.SignInHandler.bind(this)
-        this.SubmitHandler = this.SubmitHandler.bind(this)
+        this.SignInHandler = this.SignInHandler.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
 
     }
 
     SignInHandler(e) {
         e.preventDefault();
         const answers = e.target.elements;
-        this.setState({ signedIn: true, ScouterName: answers.Sname.value, EventName: answers.Ename.value, QRCode: null });
+        this.setState({ signedIn: true, ScouterName: answers.Scouter_Name.value, EventName: answers.Competition.value, QRCode: null });
         return false;
     }
 
-    SubmitHandler(e) {
-        e.preventDefault();
-        const answers = e.target.elements;
-        this.setState({
-            QRCode: <QRCode value={
-                `${this.state.ScouterName}\t` +
-                `${this.state.EventName}\t` +
-                `${answers.match.value}\t` +
-                `${answers.Num.value}\t` +
-                `${answers.Alliance.value}\t` +
-                `${answers.GeneralUp.value}\t` +
-                `${answers.GeneralLow.value}\t` +
-                `${answers.PhotosUp.value}\t` +
-                `${answers.PhotosLow.value}\t` +
-                `${answers.foul.value}\t` +
-                `${answers.tfoul.value}\t` +
-                `${answers.climbType.value}\t` +
-                `${answers.notes.value}`
-            } size={512} />
-        });
-        return false;
-    }
-
-    setSelected(id) {
-        this.setState({ selected: id });
+    handleSubmit(event) {
+        event.preventDefault();
+        const answers = event.target.elements;
+        const data = fields.map(e => answers[e]?.value);
+        const csv = csvStringify([data]);
+        download(csv, answers.Team_Number.value + '_Pit_Scout.csv');
+        event.target.reset();
     }
 
     render() {
@@ -62,7 +86,7 @@ class App extends React.Component {
                 <TabButton onClick={this.setSelected} tabId="save-page">Save</TabButton>
             </div>
       */}
-                <form action={`http://${process.env.REACT_APP_BACKEND_IP}/data/pits`} method="POST" target="frame" id="myForm" onSubmit={clearForm}>
+                <form action={`http://${process.env.REACT_APP_BACKEND_IP}/data/pits`} method="POST" id="myForm" onSubmit={this.handleSubmit}>
                     <input type='hidden' value={this.state.EventName} name='Competition' />
                     <input type='hidden' value={this.state.ScouterName} name='Scouter_Name' />
 
@@ -76,14 +100,6 @@ class App extends React.Component {
             </main>
         );
     }
-}
-
-function clearForm() {
-    document.getElementById("myForm").submit();
-    setTimeout(function () {
-        document.getElementById("myForm").reset();
-        window.location.href = "#SignIn"
-    }, 0)
 }
 
 // function TabButton(props) {
