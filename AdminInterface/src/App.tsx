b81077cb613daf0f42bc6b6fd75420ccb5bd1bf8
JosiahFu <josiahfu@gmail.com>
2023-03-11 14:33:35 -0800

    Organization and icons

--- AdminInterface/src/App.tsx
+++ AdminInterface/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import 'material-symbols/outlined.css'
 
 interface TabletStatus {
     Scouter_Name: string,
@@ -32,39 +33,53 @@ function App() {
         updateData();
     }, [])
 
+    return (<main>
+        <TabletStatusDisplay allTabletStatus={tabletStatus} />
+    </main>)
+}
+
+function TabletStatusDisplay({ allTabletStatus }: { allTabletStatus?: AllTabletStatus }) {
     const StatusCard = (
         { status }: { status?: TabletStatus }
     ) => {
         const { Scouter_Name = '', Team_Number = '', Battery_Level = 0, Online = false } = status ?? {};
 
+        const battery = Math.round(Battery_Level * 7);
+
         return (<div className="status-card">
             <div className="status-line team-number">{Team_Number}</div>
             <div className="status-line scouter-name">{Scouter_Name}</div>
             {Online
-                ? <div className="status-line battery">{Math.round(Battery_Level * 100)}%</div>
-                : <div className="status-line battery noconnect">Not Connected</div>
+                ? <div className="status-line battery">
+                    <span className="material-symbols-outlined">battery_{battery === 7 ? 'full' : battery + '_bar'}</span>
+                    {Math.round(Battery_Level * 100)}%
+                </div>
+                : <div className="status-line battery noconnect">
+                    <span className="material-symbols-outlined"> signal_disconnected</span>
+                    Not Connected
+                </div>
             }
         </div>);
     }
 
     return (
-        <main>
-            <div className="status">
-                <div className="status-red">
-                    <StatusCard status={tabletStatus?.[0]} />
-                    <StatusCard status={tabletStatus?.[1]} />
-                    <StatusCard status={tabletStatus?.[2]} />
-                    <StatusCard status={tabletStatus?.[6]} />
-                </div>
-                <div className="status-blue">
-                    <StatusCard status={tabletStatus?.[3]} />
-                    <StatusCard status={tabletStatus?.[4]} />
-                    <StatusCard status={tabletStatus?.[5]} />
-                    <StatusCard status={tabletStatus?.[7]} />
-                </div>
+        <div className="status">
+            <div className="status-red">
+                <StatusCard status={allTabletStatus?.[0]} />
+                <StatusCard status={allTabletStatus?.[1]} />
+                <StatusCard status={allTabletStatus?.[2]} />
+                <StatusCard status={allTabletStatus?.[6]} />
             </div>
-        </main>
+            <div className="status-blue">
+                <StatusCard status={allTabletStatus?.[3]} />
+                <StatusCard status={allTabletStatus?.[4]} />
+                <StatusCard status={allTabletStatus?.[5]} />
+                <StatusCard status={allTabletStatus?.[7]} />
+            </div>
+        </div>
     );
 }
+
+// function MatchDisplay({ allMatches }: {allMatches?: })
 
 export default App;
