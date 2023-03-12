f1b70f283b7ee8290dc1b0aa39a1231017a92624
JosiahFu <josiahfu@gmail.com>
2023-03-11 23:21:06 -0800

    Team Alliance in Sign-in + Auto team finding works + auto increment

--- BackendServer/python/scheduleDl.py
+++ BackendServer/python/scheduleDl.py
@@ -14,10 +14,19 @@ x = requests.get(
 )
 
 if x.status_code == 200:
-    teamKeys = [i['alliances']['red']['team_keys'] + i['alliances']['blue']['team_keys'] for i in x.json()]
-    teamNumbers = {index + 1: [int(j[3:]) for j in match] for index, match in enumerate(teamKeys)}
+    data = x.json()
+    matches = {
+        i['key'][len(EVENT_KEY) + 3:]: 
+        [
+            j[3:]
+            for j in i['alliances']['red']['team_keys'] + i['alliances']['blue']['team_keys']
+        ]
+        for i in data 
+        if i['key'][:len(EVENT_KEY) + 3] == EVENT_KEY + '_qm'
+    }
+
     out = open('schedule.json', 'w')
-    out.write(json.dumps({'matches': teamNumbers}))
+    out.write(json.dumps(matches))
     out.close()
 else:
     print('HTTP', x.status_code, x.reason)
